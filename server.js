f06c73c26a8954e8aeb270155167b442eb818642
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2017-05-22 12:42:25 -0500

    testing

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
+// var app = require('http').createServer(handler)
+// var io = require('socket.io')(app);
+// var fs = require('fs');
+// io.origins('*:*');
+// app.listen(8080);
+
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
+
+//     res.writeHead(200);
+//     res.end(data);
+
+//   });
+// }
+
+// io.on('connection', function (socket) {
+//   socket.emit('update', { datatype: 'action to take' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+//   socket.on('test-channel', function (data) {
+//     console.log(data);
+//     io.emit('test-channel', data);
+//   });
+// });
+
+
+
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
-io.origins('*:*');
-app.listen(8080);
+
+app.listen(3000);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -14,18 +47,12 @@ function handler (req, res) {
 
     res.writeHead(200);
     res.end(data);
-
   });
 }
 
 io.on('connection', function (socket) {
-  socket.emit('update', { datatype: 'action to take' });
+  socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
-  socket.on('test-channel', function (data) {
-    console.log(data);
-    io.emit('test-channel', data);
-  });
 });
-
