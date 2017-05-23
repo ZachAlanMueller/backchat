b2427554b942601da77ef4fb1ad3d7f1edcc87c4
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2017-05-23 12:47:46 -0500

    Connected to Mysql DB and added .env file

--- server.js
+++ server.js
@@ -1,42 +1,30 @@
-// var app = require('http').createServer(handler)
-// var io = require('socket.io')(app);
-// var fs = require('fs');
-// io.origins('*:*');
-// app.listen(8080);
-
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
-
-//     res.writeHead(200);
-//     res.end(data);
-
-//   });
-// }
-
-// io.on('connection', function (socket) {
-//   socket.emit('update', { datatype: 'action to take' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-//   socket.on('test-channel', function (data) {
-//     console.log(data);
-//     io.emit('test-channel', data);
-//   });
-// });
-
-
-
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+var mysql = require('mysql');
+var env = require('node-env-file');
+env(__dirname + '/.env');
+//console.log(process.env.test);
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : process.env.db_user,
+  password : process.env.db_password,
+  database : 'backchat'
+});
+
+// connection.connect();
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
+
 io.origins('*:*');
 app.listen(3000);
-
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -51,6 +39,7 @@ function handler (req, res) {
   });
 }
 
+
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   console.log('Client connected...')
@@ -62,3 +51,7 @@ io.on('connection', function (socket) {
   io.emit('testing', data);
   });
 });
+
+function getMessages(){
+
+}
